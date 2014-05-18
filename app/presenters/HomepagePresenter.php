c69e0c1d320525696545800abee40cfc7e56a878
Miroslav Mrázek <miroslav.mrazek@gmail.com>
2014-05-18 06:35:29 +0200

    Ukázka propojení presenteru a šablony na jednoduchém výpisu dat.

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -11,6 +11,21 @@ class HomepagePresenter extends BasePresenter
 
 	public function renderDefault()
 	{
+		$books = [
+				'Robert Louis Stevenson' => [
+					'Ostrov pokladů',
+					'Podivný případ Dr. Jekylla a pana Hyda',
+					'Černý šíp',
+					'Rytíř z Ballantrae'
+					],
+				'John Ronald Reuel Tolkien' => [
+					'Hobit aneb Cesta tam a zase zpátky',
+					'Pán prstenů',
+					'Silmarillion'
+					]
+				];
+		
+		$this->template->books = $books;
 	}
 
 }
