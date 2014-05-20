d2416593b1d17a26e7a1cdb22a1e9b2156ef8fd1
Miroslav Mrázek <miroslav.mrazek@gmail.com>
2014-05-20 07:26:44 +0200

    Refaktoring struktury presenterů a pohledů.

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -2,10 +2,6 @@
 
 namespace App\Presenters;
 
-use App\Model\BookRepository,
-	App\Model\AuthorRepository,
-    Nette\Application\UI\Form;
-
 
 /**
  * Homepage presenter.
@@ -13,88 +9,8 @@ use App\Model\BookRepository,
 class HomepagePresenter extends BasePresenter
 {
 	
-	/**
-	 * @var BookRepository
-	 * @inject
-	 */
-	public $books;
-	
-	/**
-	 * @var AuthorRepository
-	 * @inject
-	 */
-	public $authors;
-	
-	
-	
 	public function renderDefault()
 	{
 	}
-	
-
-	public function renderBookcase()
-	{
-		$this->template->books = $this->books->findAll();
-	}
-	
-	
-	public function actionEdit($id)
-	{
-		$book = $this->books->get($id);
-		$this['bookForm']->setDefaults($book);
-		
-	}
-
-	
-
-
-	/**
-	 * @param int
-	 */
-	public function handleDelete($id)
-	{
-		$this->books->get($id)->delete();
-		$this->redirect('this');
-	}
-	
-	
-	/**
-	 * Formulář pro zadání, nebo editaci knihy.
-	 * @return Form
-	 */
-	protected function createComponentBookForm()
-	{
-		$form = new Form;
-		$form->addHidden('id');
-		$form->addText('title', 'Název knihy')
-			->setRequired('Vyplňte název knihy.');
-		$form->addSelect('id_author', 'Autor', $this->authors->findAll()->fetchPairs('id', 'name'))
-			->setRequired('Vyberte autora knihy.');
-		$form->addSubmit('ok', 'Odeslat');
-		$form->onSuccess[] = $this->bookFormSuccess;
-
-		return $form;
-	}
-
-
-	/**
-	 * Zpracování formuláře pro zadání, nebo editaci knihy.
-	 * @param Form $form
-	 */
-	public function bookFormSuccess($form)
-	{
-		$values = $form->getValues();
-		
-		if(empty($values->id)) {
-			$this->books->insert($values);
-		}
-		else {
-			$book = $this->books->get($values->id);
-			$book->update($values);
-		}
-		
-		$this->redirect('bookcase');
-	}
-
 
 }
